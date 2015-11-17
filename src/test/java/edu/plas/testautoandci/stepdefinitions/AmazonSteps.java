f087ddb05b799046e59bf914eec579bf14742f26
David Camilleri <camilleridavid@gmail.com>
2015-11-17 21:02:27 +0100

    DC - finalised Amazon test

--- src/test/java/edu/plas/testautoandci/stepdefinitions/AmazonSteps.java
+++ src/test/java/edu/plas/testautoandci/stepdefinitions/AmazonSteps.java
@@ -19,8 +19,6 @@ public class AmazonSteps {
         WebElement search = Driver.getWebDriver().findElement(By.name("site-search"));
         search.findElement(By.id("twotabsearchtextbox")).sendKeys(item);
         search.findElement(By.className("nav-input")).click();
-
-
     }
 
     @Then("^a number of results are returned$")
@@ -46,22 +44,48 @@ public class AmazonSteps {
 
     @Then("^a message confirming item is added to Basket is shown$")
     public void aMessageConfirmingItemIsAddedToBasketIsShown() {
-
+        assertEquals("Added to Basket", Driver.getWebDriver().findElement(By.id("huc-v2-order-row-confirm-text")).getText());
+        assertTrue(Driver.getWebDriver().findElement(By.cssSelector("#hlb-subcart span.huc-subtotal")).getText().contains("(1 item)"));
     }
 
     @When("^I access the Basket$")
     public void iAccessTheBasket() {
+        Driver.getWebDriver().findElement(By.id("nav-cart")).click();
     }
 
     @Then("^the Basket contains '(\\d+)' items$")
     public void theBasketContainsItems(int numberOfItems) {
+        // Basket items number in trolley sign (top right)
+        assertEquals(String.valueOf(numberOfItems), Driver.getWebDriver().findElement(By.id("nav-cart-count")).getText());
+
+        String numberOfItemsText;
+        if (numberOfItems <= 1) {
+            numberOfItemsText = "(" + numberOfItems + " item)";
+        } else {
+            numberOfItemsText = "(" + numberOfItems + " items)";
+        }
+
+        // Basket items number in shopping basket
+        assertTrue(Driver.getWebDriver()
+                .findElement(By.cssSelector("#activeCartViewForm .sc-subtotal"))
+                .getText().contains(numberOfItemsText));
+
+        // This section is only visible when there are items in the Basket
+        if (numberOfItems > 0) {
+            // Basket items number in Subtotal above 'Proceed to Checkout' button
+            assertTrue(Driver.getWebDriver()
+                    .findElement(By.xpath("//*[@id='gutterCartViewForm']//*[@class='a-box-group']//*[contains(@class,'sc-subtotal')]//span[contains(@class,'sc-price')]/preceding-sibling::span"))
+                    .getText().contains(numberOfItemsText));
+        }
     }
 
-    @When("^the current item is displayed inside the Basket$")
+    @Then("^the current item is displayed inside the Basket$")
     public void theCurrentItemIsDisplayedInsideTheBasket() {
+        assertEquals(titleInSearchResults, Driver.getWebDriver().findElement(By.cssSelector(".sc-list-item-content .a-list-item .sc-product-link")).getText());
     }
 
     @When("^I delete the current item from the Basket$")
     public void iDeleteTheCurrentItemFromTheBasket() {
+        Driver.getWebDriver().findElement(By.cssSelector(".sc-list-item-content .sc-action-links input[value=\"Delete\"]")).click();
     }
 }
