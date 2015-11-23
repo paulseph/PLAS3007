049d271acc03d00d847704effeac78f0a94ac8c3
d.camilleri <d.camilleri@betclicgroup.com>
2015-11-23 15:06:57 +0100

    DC - adding ability to run tests on more browsers

--- src/test/java/edu/plas/testautoandci/driver/Driver.java
+++ src/test/java/edu/plas/testautoandci/driver/Driver.java
@@ -4,11 +4,17 @@ import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import edu.plas.testautoandci.helper.DriverScreenShotHelper;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
+import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -66,6 +72,40 @@ public class Driver {
                 FirefoxProfile firefoxProfile = new FirefoxProfile();
                 firefoxProfile.setEnableNativeEvents(false);
                 driver = new FirefoxDriver(firefoxProfile);
+            } else if (browser.equals("localChrome")) {
+                if (System.getProperty("os.name").contains("Mac")) {
+                    System.setProperty("webdriver.chrome.driver", "browserdriver/chrome/chromedriver");
+                } else if (System.getProperty("os.name").contains("Windows")) {
+                    System.setProperty("webdriver.chrome.driver", "browserdriver/chrome/chromedriver.exe");
+                }
+                driver = new ChromeDriver();
+            } else if (browser.equals("localIE")) {
+                System.setProperty("webdriver.ie.driver", "browserdriver/ie/IEDriverServer.exe");
+                driver = new InternetExplorerDriver();
+
+
+            } else if (browser.equals("gridFirefox")) {
+                DesiredCapabilities capability = DesiredCapabilities.firefox();
+                capability.setBrowserName("firefox");
+                capability.setVersion("41.0.1");
+                capability.setPlatform(Platform.WINDOWS);
+                FirefoxProfile firefoxProfile = new FirefoxProfile();
+                firefoxProfile.setEnableNativeEvents(false);
+                capability.setCapability(FirefoxDriver.PROFILE, firefoxProfile);
+                driver = new RemoteWebDriver(new URL("http://40.127.132.250:4444/wd/hub"), capability);
+
+            } else if (browser.equals("gridChrome")) {
+                DesiredCapabilities capability = DesiredCapabilities.chrome();
+                capability.setBrowserName("chrome");
+                capability.setPlatform(Platform.WINDOWS);
+                new RemoteWebDriver(new URL("http://40.127.132.250:4444/wd/hub"), capability);
+
+            } else if (browser.equals("gridIE")) {
+                DesiredCapabilities capability = DesiredCapabilities.internetExplorer();
+                capability.setBrowserName("iexplore");
+                capability.setVersion("11");
+                capability.setPlatform(Platform.WINDOWS);
+                new RemoteWebDriver(new URL("http://40.127.132.250:4444/wd/hub"), capability);
             } else {
                 throw new IllegalArgumentException("Browser system property is wrong! Cannot be " + browser);
             }
