66c80a4ef39e62dc16eb8d2cf5b38972632e7ba3
David Camilleri <camilleridavid@gmail.com>
2015-12-16 23:48:00 +0100

    DC - sikuli test

--- src/test/java/edu/plas/testautoandci/stepdefinitions/SikuliSteps.java
+++ src/test/java/edu/plas/testautoandci/stepdefinitions/SikuliSteps.java
@@ -1,12 +1,15 @@
 package edu.plas.testautoandci.stepdefinitions;
 
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import edu.plas.testautoandci.driver.SikuliDriver;
 import org.sikuli.script.App;
 import org.sikuli.script.FindFailed;
 import org.sikuli.script.Key;
 import org.sikuli.script.Screen;
+
+import static org.junit.Assert.assertTrue;
 
 public class SikuliSteps {
     private Screen sikuliDriver = SikuliDriver.getSikuliDriver();
@@ -16,20 +19,44 @@ public class SikuliSteps {
         App.open("Firefox");
 
         String addressBarImage = SikuliDriver.SIKULI_IMAGES_PATH + "firefox_address_bar.png";
-        sikuliDriver.wait(addressBarImage, 30);
+        sikuliDriver.wait(addressBarImage, 60);
         sikuliDriver.type(addressBarImage, url + Key.ENTER);
     }
 
-    @When("^I click on the Sikuli (.*) tab$")
-    public void iClickOnTheSikuliTab(String tab) throws FindFailed {
+    @When("^I click on the '(.*)' tab using Sikuli$")
+    public void iClickOnTheTabUsingSikuli(String tab) throws FindFailed {
         switch (tab) {
             case "Documentation":
-                sikuliDriver.click("src/test/resources/sikuliimages/documentation_tab.png");
-                try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                sikuliDriver.wait(SikuliDriver.SIKULI_IMAGES_PATH + "documentation_tab.png", 15);
+                sikuliDriver.click(SikuliDriver.SIKULI_IMAGES_PATH + "documentation_tab.png");
+                break;
+            default:
+                throw new IllegalArgumentException("Tab handling not found!");
+        }
+    }
+
+    @When("^I click on the '(.*)' link using Sikuli$")
+    public void iClickOnTheLinkUsingSikuli(String link) throws FindFailed {
+        switch (link) {
+            case "table of contents":
+                sikuliDriver.wait(SikuliDriver.SIKULI_IMAGES_PATH + "table_of_contents.png", 15);
+                sikuliDriver.click(SikuliDriver.SIKULI_IMAGES_PATH + "table_of_contents.png");
+                break;
+            default:
+                throw new IllegalArgumentException("Link handling not found!");
+        }
+    }
+
+    @Then("^the '(.*)' text is '(.*)'")
+    public void theTextIsCorrect(String sectionName, String expectedText) throws FindFailed {
+        switch (sectionName) {
+            case "Complete Guide":
+                sikuliDriver.wait(SikuliDriver.SIKULI_IMAGES_PATH + "complete_guide.png", 15);
+                assertTrue(sikuliDriver.find(SikuliDriver.SIKULI_IMAGES_PATH + "complete_guide.png").text().contains(expectedText));
+                break;
+            default:
+                System.out.println("In Default");
+                throw new IllegalArgumentException("Section handling not found!");
         }
     }
 }
--- src/test/resources/edu/plas/testautoandci/features/sikuli/Sikuli.feature
+++ src/test/resources/edu/plas/testautoandci/features/sikuli/Sikuli.feature
@@ -3,4 +3,6 @@ Feature: Testing Sikuli functionality
   @sikuli
   Scenario: eee
     Given I use Sikuli to navigate to http://www.sikuli.org/
-    When I click on the Sikuli Documentation tab
+    When I click on the 'Documentation' tab using Sikuli
+    And I click on the 'table of contents' link using Sikuli
+    Then the 'Complete Guide' text is 'What is new in SikuliX'
